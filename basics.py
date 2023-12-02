345b4f7625ef161a7d0872e38c707f5b332d9384
EcaCosca <enriquecoscarelli@gmail.com>
2023-12-02 16:02:35 +0100

    Done

---
+++ basics.py
@@ -0,0 +1,40 @@
+# # EXECUTION ORDER
+# print('line 1') # THIS WILL BE IGNORED
+# print('line 2')
+# print('line 3')
+# print('line 4')
+
+# # DATATYPES
+# print('words') # string
+# print("words")
+# print(100) # integer
+# print(-100) # integer
+# print(100.0) # float
+
+# # OPERATORS
+# print(3+3)
+# print(3-3)
+# print(3*3)
+# print(3/3)
+# print(3+3.763986324)
+# print(3/3.763986324)
+# print(10 * 'hello')
+# print('hello' * 10)
+# print(13 - 3 *4 +2)
+# print(13 - 3 * (4 +2))
+
+# # VARIABLES
+# test_value = 123
+# print(test_value)
+# test_value = test_value + 1000
+# print(test_value)
+# test_value += 20000
+# print(test_value)
+
+# INPUT
+# user_input = input('Enter a number: ')
+# print(user_input)
+
+# exercise
+user_name = input('Enter your name: ')
+print('Hello',user_name,'have a nice day!')
