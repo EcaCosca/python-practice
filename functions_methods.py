544adcfce611a928e4f2dd8383f7f38483bc552b
EcaCosca <enriquecoscarelli@gmail.com>
2023-12-06 14:42:56 +0100

    Done with exercise for video 3

---
+++ functions_methods.py
@@ -0,0 +1,14 @@
+# functions
+# print("value")
+# print(input("Enter a value: "))
+
+# methods
+print("value".upper())
+print("VALUE".lower())
+print("calue".replace("c", "v"))
+
+# new functions
+print(abs(-1))
+print(max(10,5))
+print(min(0,5))
+print(len('test'))
---
+++ pythagoras_theorem_calculator.py
@@ -0,0 +1,7 @@
+width = int(input('Enter the width number: '))
+height = int(input('Enter the height number: '))
+
+hypothenouse = pow((width**2 + height**2), 1/2)
+
+print('The hypothenouse is', hypothenouse)
+
